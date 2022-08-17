ee64853beb51d8077592f95da60e56955d6039ba
ThisKarolGajda <jestemdobrywniczym@gmail.com>
2022-08-17 16:54:47 +0200

    1.1.0

--- src/main/java/me/opkarol/opc/OpC.java
+++ src/main/java/me/opkarol/opc/OpC.java
@@ -1,11 +1,7 @@
 package me.opkarol.opc;
 
-import me.opkarol.opc.api.commands.OpCommand;
-import me.opkarol.opc.api.commands.arguments.StringArg;
 import me.opkarol.opc.api.language.LanguageDatabase;
 import me.opkarol.opc.api.plugin.OpPlugin;
-
-import java.util.Arrays;
 
 public final class OpC extends OpPlugin {
     private LanguageDatabase langDb;
@@ -25,23 +21,5 @@ public final class OpC extends OpPlugin {
         langDb.getLanguageTool().onDisable();
         langDb = null;
         super.onDisable();
-    }
-
-    void test() {
-        new OpCommand("command-name")
-                // We need to remove default command suggestion because we have to override it
-                .setRemoveDefaultCommandSuggestion(true)
-                // Add String argument with name arg and suggestion with list of animals names
-                .addArgSuggestion(new StringArg("arg"), Arrays.asList("Cat", "Dog", "Bird"))
-                .execute((sender, args) -> {
-                    String arg = (String) args.get("arg");
-                    if (arg.equalsIgnoreCase("Cat")) {
-                        sender.sender().sendMessage("I love cats!!!");
-                    } else {
-                        sender.sender().sendMessage("You have chosen: " + args.get("arg"));
-                    }
-                })
-                // Don't forget to register it!!!
-                .register();
     }
 }
