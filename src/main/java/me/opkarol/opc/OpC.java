e30a1b3b50772695c3b39a2e305face2a32b2cb9
ThisKarolGajda <jestemdobrywniczym@gmail.com>
2022-08-17 16:04:14 +0200

    1.1.0

--- src/main/java/me/opkarol/opc/OpC.java
+++ src/main/java/me/opkarol/opc/OpC.java
@@ -1,11 +1,7 @@
 package me.opkarol.opc;
 
-import me.opkarol.opc.api.gui.misc.OpInventoryRows;
-import me.opkarol.opc.api.gui.pattern.OpInventoryPattern;
-import me.opkarol.opc.api.gui.type.OpInventoryBuilder;
 import me.opkarol.opc.api.language.LanguageDatabase;
 import me.opkarol.opc.api.plugin.OpPlugin;
-import org.bukkit.Material;
 
 public final class OpC extends OpPlugin {
     private LanguageDatabase langDb;
@@ -16,13 +12,6 @@ public final class OpC extends OpPlugin {
         langDb = new LanguageDatabase();
         // OpPluginLanguage language = LanguageDatabase.addPluginLanguage(new OpPluginLanguage(getInstance(), LanguageType.en_US, LanguageType.pl_PL));
         super.onEnable();
-
-        new OpInventoryBuilder("inv", "Title", OpInventoryRows.ROWS_1)
-                .setPattern(new OpInventoryPattern("X O X O C O X O X")
-                        .addPatternValue("X", Material.BLACK_STAINED_GLASS_PANE)
-                        .addPatternValue("O", Material.WRITTEN_BOOK)
-                        .addPatternValue("C", Material.MAGENTA_TERRACOTTA))
-                .saveInventory(OpC.getInstance().getConfiguration(), "inventories");
 
     }
 
