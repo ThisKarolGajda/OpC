e2d370666b23dea2858ad49237a16dfcf7d9e7d0
ThisKarolGajda <jestemdobrywniczym@gmail.com>
2022-10-14 15:19:14 +0200

    1.4.3.9

--- src/main/java/me/opkarol/opc/api/database/mysql/base/OpMDatabaseAuto.java
+++ src/main/java/me/opkarol/opc/api/database/mysql/base/OpMDatabaseAuto.java
@@ -1,30 +1,25 @@
 package me.opkarol.opc.api.database.mysql.base;
 
 import java.util.UUID;
-import java.util.function.Function;
 import java.util.function.Predicate;
 
 public abstract class OpMDatabaseAuto<O> extends OpMDatabase<O> {
-    private final Function<O, Object> defaultSearch;
 
     public OpMDatabaseAuto(OpMSingleDatabase<O> database) {
         super(database);
-        this.defaultSearch = getDefaultSearch();
     }
 
-    public abstract Predicate<O> getPredicate(Object object, Function<O, Object> defaultSearch);
-
-    public abstract Function<O, Object> getDefaultSearch();
+    public abstract Predicate<O> getPredicate(Object object);
 
     public boolean delete(UUID uuid, Object object) {
-        return delete(uuid, getPredicate(object, defaultSearch));
+        return delete(uuid, getPredicate(object));
     }
 
     public boolean contains(UUID uuid, Object object) {
-        return contains(uuid, getPredicate(object, defaultSearch));
+        return contains(uuid, getPredicate(object));
     }
 
     public int getId(UUID uuid, Object object) {
-        return getId(uuid, getPredicate(object, defaultSearch));
+        return getId(uuid, getPredicate(object));
     }
 }
