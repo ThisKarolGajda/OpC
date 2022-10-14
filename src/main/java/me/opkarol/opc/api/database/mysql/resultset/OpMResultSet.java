aecf9bfbe483a18e78d2569f304d8956a41360e8
ThisKarolGajda <jestemdobrywniczym@gmail.com>
2022-10-14 16:16:00 +0200

    1.4.3.12

--- src/main/java/me/opkarol/opc/api/database/mysql/resultset/OpMResultSet.java
+++ src/main/java/me/opkarol/opc/api/database/mysql/resultset/OpMResultSet.java
@@ -1,20 +1,21 @@
 package me.opkarol.opc.api.database.mysql.resultset;
 
 import me.opkarol.opc.api.location.OpSerializableLocation;
-import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.NotNull;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.UUID;
 
 public record OpMResultSet(ResultSet set) {
 
-    public @Nullable String getText(String column) {
+    public String getText(String column) {
         try {
             return set.getString(column);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return null;
+        return "";
     }
 
     public int getInt(String column) {
@@ -26,12 +27,16 @@ public record OpMResultSet(ResultSet set) {
         return 1;
     }
 
-    public @Nullable OpSerializableLocation getLocation(String column) {
+    public @NotNull OpSerializableLocation getLocation(String column) {
         try {
             return new OpSerializableLocation(set.getString(column));
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return null;
+        return new OpSerializableLocation();
+    }
+
+    public @NotNull UUID getUUID(String column) {
+        return UUID.fromString(getText(column));
     }
 }
